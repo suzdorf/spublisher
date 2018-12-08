78672b5a409132ea300ca10be0e794e5d1e8ac34
Dmitriy Suzdorf <d.suzdorf@gmail.com>
2018-12-08 19:08:54 +0300

    F5: Create "spublisher" application
    * T46: Create Wix installer

---
+++ Installer/Installer/Program.cs
@@ -0,0 +1,32 @@
+﻿using WixSharp;
+using WixSharp.CommonTasks;
+
+namespace Installer
+{
+    class Script
+    {
+        public static void Main(string[] args)
+        {
+            var pathVariable =
+                new EnvironmentVariable("PATH", "[INSTALLDIR]");
+
+            pathVariable.System = true;
+            pathVariable.Action = EnvVarAction.set;
+            pathVariable.Part = EnvVarPart.last;
+            pathVariable.Permanent = true;
+
+            var project = new Project("spublisher",
+                new Dir(@"%ProgramFiles%\spublisher",
+                    new DirFiles(@"spublisher\*.dll*"),
+                    new File(@"spublisher\SPublisher.exe"),
+                    new File(@"spublisher\SPublisher.exe.config")),
+                pathVariable);
+
+            project.SetVersionFromFile(@"spublisher\SPublisher.exe");
+            project.LicenceFile = "licence.rtf";
+            project.ScheduleReboot = new ScheduleReboot{ InstallSequence = RebootInstallSequence.Both};
+
+            Compiler.BuildMsi(project);
+        }
+    }
+}
--- Installer/Installer/Properties/AssemblyInfo.cs
+++ Installer/Installer/Properties/AssemblyInfo.cs
@@ -1,34 +1,36 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SPublisher")]
+[assembly: AssemblyTitle("Installer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SPublisher")]
+[assembly: AssemblyProduct("Installer")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2ea994b3-d684-4ecc-ac1a-102db5cdf83b")]
+[assembly: Guid("b3ddde34-0e54-4f10-9bd9-1c6aba0f6405")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
